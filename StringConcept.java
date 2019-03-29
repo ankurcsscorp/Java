551416abc80c6bc81f1ce1a9422ae0c506d41c20
ankurcsscorp <48344394+ankurcsscorp@users.noreply.github.com>
2019-03-29 13:03:04 +0530

    28 march

---
+++ StringConcept.java
@@ -0,0 +1,68 @@
+public class StringConcept
+
+
+{
+ 
+ 
+  public static void main(String [] args)
+
+  
+  {
+
+int k=0;
+  
+ 
+      String st="abcd";
+ 
+  
+     String str[]={"donkey","cab","apple"};
+
+ String newStr[]= new String[str.length];
+  
+    char c[]=st.toCharArray();
+  
+  
+   
+     for(int i=0;i<c.length;i++)
+  
+ 
+     {
+    
+    
+    
+       for(int j=0;j<str.length;j++)
+
+  
+      {
+ 
+    
+       if(st.charAt(i)==str[j].charAt(0))
+
+          
+        {
+	
+newStr[k]=str[j];
+
+	k++;
+
+} 
+ 
+  }
+ 
+     
+
+  }
+
+
+	for(int i=0;i<newStr.length;i++)
+
+
+	{
+	
+    System.out.println(newStr[i]);
+
+	}
+ 
+}
+
+}
